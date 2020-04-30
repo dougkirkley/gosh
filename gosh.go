7a6d14053849ac6919b5a1c7ce2f503fc21402b8
Douglass Kirkley <doug.kirkley@gmail.com>
2020-04-30 13:39:28 -0400

    Update gosh.go

--- gosh.go
+++ gosh.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"strings"
-	_"time"
 	"bufio"
 	"io"
 	"io/ioutil"
@@ -18,11 +17,8 @@ import (
 )
 
 var (
-    Hosts         = hosts.ReadHosts()
+        Hosts         = hosts.ReadHosts()
 	results       = make(chan []string, 150)
-	puppetArgs    = "--onetime --verbose --no-daemonize --ignorecache --no-usecacheonfailure --no-splay"
-	puppetCmd     = "cd /tmp; sudo /usr/bin/puppet agent"
-	defaultCmd    = fmt.Sprintf("%s %s", puppetCmd, puppetArgs)
 )
 
 func isFlagPassed(name string) bool {
@@ -37,20 +33,14 @@ func isFlagPassed(name string) bool {
 }
 
 func main() {
-	Cmd := flag.String("c", defaultCmd, "Command to Run on Remote Nodes")
+	Cmd := flag.String("c", "", "Command to Run on Remote Nodes")
 	Hosts := flag.String("H", strings.Join(hosts, ","), "Hosts to Run Command on")
-	Quiet := flag.Bool("q", false, "Hide Output From Commands")
 	flag.Parse()
-	if cmp.Equal(hosts, Hosts) {
-		fmt.Println("No Host entered. Running on all detected servers")
-		fmt.Print("Running on all servers")
-	}
 	for _, hostname := range hosts {
 		go func(hostname string) {
 			c := &Connection{
 				Host:    hostname,
 				Command: *Cmd,
-				Quiet:   *Quiet,
 				Timeout: *Timeout,
 			}
 			results <- c.ExecuteCmd()
